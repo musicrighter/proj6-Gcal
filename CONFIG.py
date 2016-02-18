7affc95abf3876c70bbd162ad59b791fcf298f03
David Gustafson <djg@ix-trusty.cs.uoregon.edu>
2016-02-17 17:38:10 -0800

    things not there

---
+++ CONFIG.py
@@ -0,0 +1,18 @@
+"""
+Configuration of 'memos' Flask app. 
+Edit to fit development or deployment environment.
+
+"""
+import random 
+
+### My local development environment
+#PORT=5000
+#DEBUG = True
+
+
+### On ix.cs.uoregon.edu (Michal Young's instance of MongoDB)
+#PORT=random.randint(5000,8000)
+PORT=5678
+DEBUG = False # Because it's unsafe to run outside localhost
+GOOGLE_LICENSE_KEY = ".goog_app_key.json"
+
